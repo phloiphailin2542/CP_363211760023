78f15a1abe4fa262fd5420375e695cd0a26a6610
phloiphailin <phloiphailin.k@rmutsvmail.com>
2020-07-15 14:56:20 +0700

    add more code

--- src/Lad1/HelloWorld.java
+++ src/Lad1/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("พลอยไลิน ขาวแก้ว");
+        System.out.println("รหัสนักศึกษา 363211760023");
+        System.out.println("สาขา การจัดการเทคโนโลยีการจัดการ");
+        System.out.println("คณะ การจัดการเทคโนโลยีสารสนเทศ");
+
 
 
 
